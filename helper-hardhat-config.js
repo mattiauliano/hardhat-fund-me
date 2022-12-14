610b897e8f1ca6b6fb5953dc30861537d054044e
Mattia Uliano <mattiauliano308@gmail.com>
2022-12-14 18:29:58 +0100

    Add constants to module.exports

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -5,10 +5,19 @@ const networkConfig = {
         name: "goerli",
         // Pairs price feed addresses
         ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
-        btcUsdPriceFeed: "0xA39434A63A52E749F02807ae27335515BA4b07F7",
+        btcUsdPriceFeed: "0xA39434A63A52E749F02807ae27335515BA4b07F7"
     }
-}
+};
+
+// Development chains to mocking
+const developmentChains = ["hardhat", "localhost"];
+// Adding fake decimals and value to pass them in mock constructor
+const DECIMALS = 8;
+const INITIAL_ANSWER = 100000000000;
 
 module.exports = {
     networkConfig,
-}
+    developmentChains,
+    DECIMALS,
+    INITIAL_ANSWER
+};
