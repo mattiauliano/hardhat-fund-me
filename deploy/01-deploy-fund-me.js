773081df75f15089278472fabc811aa99260edb7
Mattia Uliano <mattiauliano308@gmail.com>
2022-12-14 13:38:29 +0100

    Set priceFeedAddress using chainId and helper

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,3 +1,6 @@
+// Get access to helper (handling multiple networks and addresses)
+const { networkConfig } = require("../helper-hardhat-config");
+
 // Parameter passed in this async function is 'hre' (hardhat)
 // Destructuring 'hre' to get 'getNamedAccounts' and 'deployments' object
 module.exports = async ({ getNamedAccounts, deployments }) => {
@@ -6,8 +9,20 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     // getNamedAccounts function returns an object
     // Destructuring to get only the first name 'deployer'
     const { deployer } = await getNamedAccounts();
-    // Taking chainId for flexible conditionals
+    // Taking chainId for network conditionals
     const chainId = network.config.chainId;
 
-    
+    // Use networkConfig and chainId to handle addresses
+    const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+    const btcUsdPriceFeedAddress = networkConfig[chainId]["btcUsdPriceFeed"];
+
+    // Actually deploy FundMe contract
+    const fundMe = await deploy("FundMe", {
+        // Named account
+        from: deployer,
+        // Constructor args (price feed address)
+        args: [ethUsdPriceFeedAddress],
+        // Log transaction
+        log: true,
+    })
 };
