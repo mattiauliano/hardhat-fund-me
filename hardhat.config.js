c62f9d28004e464a97c47a237cf86702e6d40719
Mattia Uliano <mattiauliano308@gmail.com>
2022-12-14 17:32:30 +0100

    Multiple solidity compilers

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,25 +4,28 @@ require("hardhat-deploy");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.8",
-  networks: {},
-  gasReporter: {
-    enabled: false,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
-  // getNamedAccounts function returns:
-  namedAccounts: {
-    // Named account
-    deployer: {
-      default: 0,
-      // 4: 1, Ex. For goerli network is the second account
+    // Adding multiple solidity compiler versions
+    solidity: {
+        compilers: [{ version: "0.8.8" }, { version: "0.6.6" }]
     },
-    // Named account
-    user: {
-      default: 1,
+    networks: {},
+    gasReporter: {
+        enabled: false,
+        currency: "USD"
     },
-  }
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY
+    },
+    // getNamedAccounts function returns:
+    namedAccounts: {
+        // Named account
+        deployer: {
+            default: 0
+            // 5: 1, Ex. For goerli network is the second account
+        },
+        // Named account
+        user: {
+            default: 1
+        }
+    }
 };
