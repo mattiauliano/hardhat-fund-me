46993078574faa172dc3772d8eda085cdb199697
Mattia Uliano <mattiauliano308@gmail.com>
2022-12-14 19:29:20 +0100

    Add goerli network and private key

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
 // Task to deploy contracts
 require("hardhat-deploy");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+
 module.exports = {
     // Adding multiple solidity compiler versions
     solidity: {
         compilers: [{ version: "0.8.8" }, { version: "0.6.6" }]
     },
-    networks: {},
+    networks: {
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5
+        }
+    },
     gasReporter: {
         enabled: false,
         currency: "USD"
@@ -20,8 +29,9 @@ module.exports = {
     namedAccounts: {
         // Named account
         deployer: {
-            default: 0
-            // 5: 1, Ex. For goerli network is the second account
+            default: 0,
+            // For goerli network is the first account
+            5: 0
         },
         // Named account
         user: {
