4a319c41e15a426308083254d2f0b835b2cc924f
Mattia Uliano <mattiauliano308@gmail.com>
2022-12-14 17:34:20 +0100

    Import mock contract for local testing

---
+++ contracts/test/MockV3Aggregator.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+// Same solidity version of the imported file
+pragma solidity ^0.6.6;
+
+// Importing mock from chainlink/contracts package
+import "@chainlink/contracts/src/v0.6/tests/MockV3Aggregator.sol";
