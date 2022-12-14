71ebc1d3b824ed7e6a4ab81c807dcdedcdaad76f
Mattia Uliano <mattiauliano308@gmail.com>
2022-12-14 12:11:53 +0100

    Add getNamedAccounts func's return

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,4 +5,24 @@ require("hardhat-deploy");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.8",
+  networks: {},
+  gasReporter: {
+    enabled: false,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  // getNamedAccounts function returns:
+  namedAccounts: {
+    // Named account
+    deployer: {
+      default: 0,
+      // 4: 1, Ex. For goerli network is the second account
+    },
+    // Named account
+    user: {
+      default: 1,
+    },
+  }
 };
