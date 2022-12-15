13828b5ae33b0992c350e4ae891fd0118d694c8b
Mattia Uliano <mattiauliano308@gmail.com>
2022-12-15 19:50:27 +0100

    Test fund function

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -1,15 +1,54 @@
 const { deployments, ethers, getNamedAccounts } = require("hardhat");
+const { assert, expect } = require("chai"); // Overwritten by waffle
 
 describe("FundMe", () => {
     let fundMe;
     let deployer;
-    // Deploying contracts before each test
+    let mockV3Aggregator;
+    // Convert 1ETH in 1000000000000000000
+    const sendValue = ethers.utils.parseEther("1");
+    // Deploy contracts before each test
     beforeEach(async () => {
-        // Getting only deployer object and assign it to deployer variable
+        // Get only deployer object and assign it to deployer variable
         deployer = (await getNamedAccounts()).deployer;
-        // Deploying contracts with "all" tag
+        // Deploy contracts with "all" tag
         await deployments.fixture(["all"]);
-        // Getting contract using ethers-hardhat
-        fundMe = await ethers.getContract("FundMe");
+        // Get contract using ethers-hardhat
+        fundMe = await ethers.getContract("FundMe", deployer);
+        mockV3Aggregator = await ethers.getContract(
+            "MockV3Aggregator",
+            deployer
+        );
+    });
+
+    // Test constructor function
+    describe("constructor", () => {
+        it("Sets the aggregator addresses correctly", async () => {
+            const response = await fundMe.priceFeed();
+            assert.equal(response, mockV3Aggregator.address);
+        });
+    });
+
+    // Test fund function
+    describe("fund", () => {
+        it("Fails if you don't send enough ETH", async () => {
+            // Specific revert message from FundMe.sol
+            await expect(fundMe.fund()).to.be.revertedWith(
+                "Send at least 10$!"
+            );
+        });
+        it("Updates the amount founded data structure", async () => {
+            // Fund 1ETH from deployer by default (to check)
+            await fundMe.fund({ value: sendValue });
+            // Get deployer's funds
+            const response = await fundMe.addressToAmountFunds(deployer);
+            assert.equal(response.toString(), sendValue.toString());
+        });
+        it("Adds funder to array of funders", async () => {
+            await fundMe.fund({ value: sendValue });
+            // funders(0) === funders[0]
+            const pushedFunder = await fundMe.funders(0);
+            assert.equal(pushedFunder, deployer);
+        });
     });
 });
